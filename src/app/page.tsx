92f8adc9fa166a9778481a4a75e5e038ece3354a
BeardazardTCG <david.laird88@gmail.com>
2025-06-12 15:40:41 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
     siteName: "PokéBinder",
     images: [
       {
-        url: "https://www.pokebinder.co.uk/banner.png",
+        url: "https://www.pokebinder.co.uk/pokebinder-logo.png",
         width: 1200,
         height: 630,
         alt: "PokéBinder Logo Banner"
