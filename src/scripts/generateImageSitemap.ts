6be51ca49094b8a55fd9135bde993e329dcc9243
BeardazardTCG <david.laird88@gmail.com>
2025-06-17 13:30:30 +0100

    Create generateImageSitemap.ts

---
+++ src/scripts/generateImageSitemap.ts
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+import { getAllCardsWithImages } from '@/lib/db';
+
+const siteUrl = 'https://www.pokebinder.co.uk';
+
+async function run() {
+  const cards = await getAllCardsWithImages(); // returns: [{ unique_id, card_name, card_image_url }]
+
+  const entries = cards.map((card) => {
+    return `
+  <url>
+    <loc>${siteUrl}/card/${card.unique_id}</loc>
+    <image:image>
+      <image:loc>${card.card_image_url}</image:loc>
+      <image:title>${card.card_name}</image:title>
+      <image:caption>${card.card_name}</image:caption>
+    </image:image>
+  </url>`;
+  });
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+${entries.join('\n')}
+</urlset>`;
+
+  const filepath = path.join(process.cwd(), 'public', 'image-sitemap.xml');
+  fs.writeFileSync(filepath, xml.trim());
+  console.log('✅ image-sitemap.xml generated.');
+}
+
+run();
