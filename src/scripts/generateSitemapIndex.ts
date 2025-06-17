a60c320001aa74844090c16fc3f78d646fc3f4f6
BeardazardTCG <david.laird88@gmail.com>
2025-06-17 17:50:59 +0100

    Update generateSitemapIndex.ts

--- src/scripts/generateSitemapIndex.ts
+++ src/scripts/generateSitemapIndex.ts
@@ -5,22 +5,23 @@ async function generateSitemapIndex() {
   const baseUrl = 'https://www.pokebinder.co.uk';
 
   const sitemapFiles = [
-    '/sitemap.xml',
     '/card-sitemap.xml',
     '/image-sitemap.xml',
-    // Add more here as needed, e.g. '/blog-sitemap.xml'
+    // Future additions like '/blog-sitemap.xml' can be added here
   ];
 
-  const urls = sitemapFiles.map(file => {
-    return `<sitemap><loc>${baseUrl}${file}</loc></sitemap>`;
-  }).join('\n');
+  const urls = sitemapFiles
+    .map(file => `<sitemap><loc>${baseUrl}${file}</loc></sitemap>`)
+    .join('\n');
 
   const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls}
 </sitemapindex>`;
 
-  fs.writeFileSync(path.join(process.cwd(), 'public/sitemap-index.xml'), sitemapIndex);
+  const outputPath = path.join(process.cwd(), 'public', 'sitemap-index.xml');
+  fs.writeFileSync(outputPath, sitemapIndex);
+
   console.log('✅ sitemap-index.xml generated.');
 }
 
