2fd4b23e2e33359c23016522e4e379ef619a53c6
BeardazardTCG <david.laird88@gmail.com>
2025-06-22 20:09:01 +0100

    Update route.ts

--- src/app/robots.txt/route.ts
+++ src/app/robots.txt/route.ts
@@ -6,10 +6,11 @@ export const dynamic = 'force-dynamic';
 export async function GET() {
   const content = `User-agent: *
 Disallow: /api/
-
+Allow: /api/cards-sitemap
 Allow: /
 
 Sitemap: https://www.pokebinder.co.uk/sitemap-index.xml
+Sitemap: https://www.pokebinder.co.uk/api/cards-sitemap
 `;
 
   return new NextResponse(content, {
